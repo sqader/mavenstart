bd2e70c0606f7f946117c4e499c221af7dd8a9c6
sqader <122376732+sqader@users.noreply.github.com>
2023-12-04 11:57:40 +0100

    first commit

---
+++ src/main/java/io/github/mat3e/App.java
@@ -0,0 +1,13 @@
+package io.github.mat3e;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        var logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World");
+    }
+}
